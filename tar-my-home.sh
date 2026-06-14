c34db002902a2b875fe0a6afcde4b0f806d1a7c6
Kovács Péter <peter.dunay.kovacs@gmail.com>
2026-06-14 11:15:40 +0200

    Add tar-my-home.sh + inventory updates

---
+++ tar-my-home.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+tar cf home.tar \
+    .bash_eternal_history \
+    .cf-api-token \
+    .claude \
+    .claude.json \
+    .codex \
+    .docker \
+    Documents \
+    .dotnet \
+    Downloads \
+    .fonts \
+    .gitconfig \
+    github \
+    .gnupg \
+    go \
+    .indicator-sysmonitor.json \
+    .kube \
+    .npm \
+    .nvm \
+    Pictures \
+    .postgres.secrets \
+    rec-zoho-test1.txt.enc \
+    rec-zoho.txt \
+    rec-zoho.txt.enc \
+    workspace \
+    .xcaliapp-cf-conf
